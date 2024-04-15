ab25e12c6db76e2dee015a038566f719a629859f
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-04-15 10:14:04 +0200

    vendor: project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Juul Van de Velde | Full Stack Developer',
+  description:
+    'Full Stack Developer student from Belgium, passionate about technology and design. Driven by a love for progress and innovation. Seeking internship opportunities in web development.',
+}
+
+// TODO: Add a favicon
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      Hier komt mijn portfolio
+    </main>
+  )
+}
