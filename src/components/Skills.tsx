0c038672d1020c02400b0f33456ca92055f68cb9
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-04-16 16:50:48 +0200

    feat: made skills section functional

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,35 +1,69 @@
+'use client'
+
+import { useState } from 'react'
 import Image from 'next/image'
 import Section from './Section'
 
 function Skills() {
+  const [currentHover, setCurrentHover] = useState<string | null>(null)
+
+  const skills = [
+    {
+      name: 'UX/UI Design',
+      image: '/assets/ui-design.jpg',
+      span: 'figma / adobe xd / wireframing / user testing',
+      alt: 'A designer working on a laptop',
+    },
+    {
+      name: 'Frontend Development',
+      image: '/assets/frontend.jpg',
+      span: 'html / css / typescript / react / next.js',
+      alt: 'A developer working on a laptop',
+    },
+    {
+      name: 'Backend Development',
+      image: '/assets/backend.png',
+      span: '.net / azure / firebase / node.js',
+      alt: 'A developer working on a laptop',
+    },
+    {
+      name: 'Flutter Development',
+      image: '/assets/flutter.jpg',
+      span: 'dart / flutter',
+      alt: 'A developer working on a laptop',
+    },
+  ]
+
+  const currentSkill = skills.find(skill => skill.name === currentHover)
+
   return (
     <Section title="WHAT I DO" id="skills">
       <div className="my-20 flex flex-row items-center justify-center gap-20">
         <div className="w-1/2">
           <Image
-            src="/assets/backend-development.png"
+            src={currentSkill ? currentSkill.image : '/assets/backend.png'}
             alt="A datacenter"
-            width={656}
-            height={437}
-            className="w-full rounded pb-2"
+            width={800}
+            height={400}
+            className="mb-2 max-w-3xl rounded"
           />
           <span className="text-2xl uppercase italic tracking-widest">
-            html / css / typescript / react / next.js
+            {currentSkill
+              ? currentSkill.span
+              : 'html / css / typescript / react / next.js'}
           </span>
         </div>
-        <ul className="text-5xl font-bold">
-          <li className="text-secondary hover:text-foreground mb-8">
-            UX/UI Design
-          </li>
-          <li className="text-secondary hover:text-foreground mb-8">
-            Frontend Development
-          </li>
-          <li className="text-secondary hover:text-foreground mb-8">
-            Backend Development
-          </li>
-          <li className="text-secondary hover:text-foreground mb-8">
-            Flutter Development
-          </li>
+        <ul className="cursor-default text-5xl font-bold">
+          {skills.map(skill => (
+            <li
+              key={skill.name}
+              className="py-4 text-secondary hover:text-foreground"
+              onMouseEnter={() => setCurrentHover(skill.name)}
+              onMouseLeave={() => setCurrentHover(null)}
+            >
+              {skill.name}
+            </li>
+          ))}
         </ul>
       </div>
     </Section>
