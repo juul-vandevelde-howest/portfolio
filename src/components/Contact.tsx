294827ddc68a9287f99e328c2cc484285d84395e
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-04-16 20:12:40 +0200

    feat: made the copy email of contact section functional

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,49 @@
 'use client'
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { gsap } from 'gsap'
 
 function Contact() {
+  const [isHovered, setIsHovered] = useState(false)
+  const [isCopied, setIsCopied] = useState(false)
+  const [xPercent, setXPercent] = useState(0)
+
+  const handleMouseEnter = () => {
+    setIsHovered(true)
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
+    setIsCopied(false)
+  }
+
+  const handleClick = () => {
+    navigator.clipboard.writeText('vandeveldejuul@protonmail.com')
+    setIsCopied(true)
+  }
+
   const firstText = useRef(null)
   const secondText = useRef(null)
   const thirdText = useRef(null)
   const fourthText = useRef(null)
   const slider = useRef(null)
 
-  let xPercent = 0
+  const xPercentRef = useRef(0)
+
+  const animate = useCallback(() => {
+    if (xPercentRef.current > 0) {
+      xPercentRef.current = -100
+    }
+    if (firstText.current)
+      gsap.set(firstText.current, { xPercent: xPercentRef.current })
+    if (secondText.current)
+      gsap.set(secondText.current, { xPercent: xPercentRef.current })
+    if (thirdText.current)
+      gsap.set(thirdText.current, { xPercent: xPercentRef.current })
+    if (fourthText.current)
+      gsap.set(fourthText.current, { xPercent: xPercentRef.current })
+    requestAnimationFrame(animate)
+    xPercentRef.current += 0.05
+  }, [])
 
   useEffect(() => {
     if (secondText.current) {
@@ -28,43 +62,39 @@ function Contact() {
       })
     }
     requestAnimationFrame(animate)
-  })
-
-  const animate = () => {
-    if (xPercent > 0) {
-      xPercent = -100
-    }
-    gsap.set(firstText.current, { xPercent: xPercent })
-    gsap.set(secondText.current, { xPercent: xPercent })
-    gsap.set(thirdText.current, { xPercent: xPercent })
-    gsap.set(fourthText.current, { xPercent: xPercent })
-    requestAnimationFrame(animate)
-    xPercent += 0.05
-  }
+  }, [animate])
 
   return (
     <div
       ref={slider}
-      className="-mx-16 mb-40 flex cursor-pointer overflow-hidden whitespace-nowrap text-8xl font-bold italic"
+      className={`-mx-16 mb-40 flex cursor-pointer overflow-hidden whitespace-nowrap text-8xl font-bold italic ${isHovered && 'w-screen justify-center'}`}
       onMouseEnter={() => {
-        gsap.to(slider.current, { duration: 1, xPercent: -100 })
+        handleMouseEnter()
       }}
       onMouseLeave={() => {
-        gsap.to(slider.current, { duration: 1, xPercent: 0 })
+        handleMouseLeave()
       }}
     >
-      <p ref={firstText} className="inline-block pr-8">
-        REACH OUT -
-      </p>
-      <p ref={secondText} className="inline-block pr-8">
-        REACH OUT -
-      </p>
-      <p ref={thirdText} className="inline-block pr-8">
-        REACH OUT -
-      </p>
-      <p ref={fourthText} className="inline-block pr-8">
-        REACH OUT -
-      </p>
+      {!isHovered ? (
+        <>
+          <p ref={firstText} className="m-0 inline-block pr-8">
+            REACH OUT -
+          </p>
+          <p ref={secondText} className="m-0 inline-block pr-8">
+            REACH OUT -
+          </p>
+          <p ref={thirdText} className="m-0 inline-block pr-8">
+            REACH OUT -
+          </p>
+          <p ref={fourthText} className="m-0 inline-block pr-8">
+            REACH OUT -
+          </p>
+        </>
+      ) : (
+        <button className="w-full italic" onClick={handleClick}>
+          {isCopied ? 'EMAIL COPIED :)' : 'CLICK TO COPY EMAIL'}
+        </button>
+      )}
     </div>
   )
 }
