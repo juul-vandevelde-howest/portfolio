8c0d02ccb8ac86d6ccc9b95fe74f6c870ecbded6
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-05-04 18:46:19 +0200

    test: added priority to skills images to check if they are more responsive

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -50,6 +50,7 @@ function Skills() {
             alt="A dog with glasses doing work on an ipad"
             className="mb-2"
             placeholder="blur"
+            priority={true}
           />
           <span className="text-nowrap uppercase italic tracking-widest lg:text-lg xl:text-2xl">
             {currentSkill
