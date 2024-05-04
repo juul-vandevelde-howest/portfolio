1118ca3b6532ec1b457930578038320f5b3b8ae0
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2024-05-04 17:37:50 +0200

    fix: removed unused code

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,28 +1,7 @@
-'use client'
-
-import { Copy } from '@phosphor-icons/react'
 import Image from 'next/image'
-import { useState } from 'react'
 import foldImage from '../../public/assets/juul.png'
 
 function About() {
-  const [isHovered, setIsHovered] = useState(false)
-  const [isCopied, setIsCopied] = useState(false)
-
-  const handleMouseEnter = () => {
-    setIsHovered(true)
-  }
-
-  const handleMouseLeave = () => {
-    setIsHovered(false)
-    setIsCopied(false)
-  }
-
-  const handleClick = () => {
-    navigator.clipboard.writeText('vandeveldejuul@protonmail.com')
-    setIsCopied(true)
-  }
-
   return (
     <section
       id="about"
@@ -53,32 +32,6 @@ function About() {
         </article>
         <div className="text-gradient absolute z-50 w-full whitespace-nowrap font-bold uppercase italic sm:pt-10 sm:text-5xl md:pt-14 md:text-6xl lg:pt-16 lg:text-7xl xl:pt-20 xl:text-8xl">
           full stack developer
-          {/* <button
-            className="flex flex-row items-center justify-start whitespace-nowrap font-bold italic lg:mt-14 lg:text-4xl xl:mt-20 xl:text-5xl"
-            onClick={() => {
-              handleClick()
-            }}
-            onMouseEnter={() => {
-              handleMouseEnter()
-            }}
-            onMouseLeave={() => {
-              handleMouseLeave()
-            }}
-          >
-            {isHovered ? (
-              <div className="flex flex-row items-center justify-start gap-2 uppercase">
-                {isCopied ? (
-                  'email copied :)'
-                ) : (
-                  <>
-                    copy email <Copy size={46} weight="fill" />
-                  </>
-                )}
-              </div>
-            ) : (
-              <div className="uppercase">reach out -&gt;</div>
-            )}
-          </button> */}
         </div>
       </div>
       <Image
